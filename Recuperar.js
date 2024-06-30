2c20e64637e4378d008aebb4a90dc7e7ec79a496
JorgeFuentes4124 <jorgee2007fc@gmail.com>
2024-06-30 17:10:19 -0300

    Update Recuperar.js

--- Recuperar.js
+++ Recuperar.js
@@ -20,12 +20,34 @@ function Ordenar_Array() {
     console.log(array);
 }
 
+//"3)Estructura de control"
+//La Estructura de control son los componentes que determinan como se ejecutan las distintas partes de un programa bajo condiciones
+//Existe 2 tipo:
+//La Estructura de control Condicional:
+//Permite tomar desiciones basadas en condiciones V o F, 
+//si es V el programa ejecuta una accion especfica
+//si es F el programa ejecuta una acción diferente
 
+//Existen 2 tipos:
+//if/else
+//La estrcutura if/else evalua una condición, si es verdadera ejecuta un bloque de codigo,
+//si es falsa ejecuta un bloque de código diferente
 
+//switch
+//Evalua una expresión y ejecuta un bloque de codigo según el valor de la expresión
 
+//Estructura de control Iterativas
+//Permiten repetir un bloque de codigo varias veces
 
+//Existen 3 tipos:
+//for:
+//Repite un bloque de codigo un numero fijo de veces
 
+//while:
+//Repite un bloque de código mientras una condición sea verdadera 
 
+//do-while
+//Similar a while, pero asegura que el bloque se ejecute al menos una vez
 
 
 
