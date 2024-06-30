9141999d914651c08a28c3bc122e290de9710988
JorgeFuentes4124 <jorgee2007fc@gmail.com>
2024-06-30 16:34:41 -0300

    Update Recuperar.js

--- Recuperar.js
+++ Recuperar.js
@@ -1,22 +1,26 @@
 
 "2)Ordenar un array=[1,0,2,4,7,6,8,12]"
 
-let array = [1, 0, 2, 4, 7, 6, 8, 12];
 
-for (let i = 0; i < array.length - 1; i++) {
-    
-    for (let j = 0; j < array.length - 1 - i; j++) {
-        
-        if (array[j] > array[j + 1]) {
-            let temp = array[j];
-            array[j] = array[j + 1];
-            array[j + 1] = temp;
+function Ordenar_Array() {
+   let array = [1, 0, 2, 4, 7, 6, 8, 12];
+
+   
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = 0; j < array.length - 1 - i; j++) {
+            if (array[j] > array[j + 1]) {
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
         }
     }
+
+   
+    console.log(array);
 }
 
-
-console.log(array);
+document.getElementById('Ordenar_Array').addEventListener('click', Ordenar_Array);
 
 
 
