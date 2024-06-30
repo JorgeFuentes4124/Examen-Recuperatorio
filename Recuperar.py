9416272acf0697ac48b8c9427a7c1a86272905fe
JorgeFuentes4124 <jorgee2007fc@gmail.com>
2024-06-30 16:08:04 -0300

    Update Recuperar.py

---
+++ Recuperar.py
@@ -0,0 +1,38 @@
+"1)Suceción Fibonacci(100 numeros)"
+"2)Ordenar un array=[1,0,2,4,7,6,8,12]"
+"3)Estructura de control"
+"4)Variables let, var y const, explicar"
+
+"1)Suceción Fibonacci (100 numeros):"
+
+j, f = 0, 1
+
+while f < 100:
+    print(f)
+
+    j,f = f, j + f
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
